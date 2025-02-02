3f0e489163e67eab06c2721b403deaebcb2bc42c
Rami Ferjani <ramiferjani.20@gmail.com>
2025-02-02 22:36:51 +0000

    Delete exercice 4.py

--- exercice 4.py
+++
@@ -1,6 +0,0 @@
-x = float ( input (" entre le valeur de ' x ' " ))
-y  = float ( input (" entre le valeur de  ' y ' " ) )
-z = float ( input (" entre le valeur de ' z '  " ) )
-
-max_n = max( x , y , z )
-print (f" le maximum des {x} , {y} et de {z} est {max_n:.2f} " ) 
