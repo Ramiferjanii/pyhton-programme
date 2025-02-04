095335f94085914c852e3c5db7542bd29901468f
Rami Ferjani <ramiferjani.20@gmail.com>
2025-02-04 21:13:21 +0000

    Delete exercice 8.py

--- exercice 8.py
+++
@@ -1,5 +0,0 @@
-annee = int ( input (" entre une annee :" ))
-if (annee % 4 == 0 and annee % 100 != 0 ) or annee % 400 == 0 :
-              print (f"l'annee {annee} et une anne bissextile " )
-else :
-    print (f"l'anne  {annee} est n'est pas bissextile ") 
