06e3eb2ebd8d34c5527e606f733a6efd8a2cb108
Rami Ferjani <ramiferjani.20@gmail.com>
2025-02-04 21:12:56 +0000

    Delete exerecice 2.py

--- exerecice 2.py
+++
@@ -1,7 +0,0 @@
-import math 
-
-a =  float ( input ( " saisir le rayon d'un cercle " ))
-surface = 2 * math.pi * a
-perimetre = (math.pi * math.pi ) * ( a * a)
-print ( f" la surface de la cercle et : {surface:.2f} " )
-print ( f" la perimetre de la cercle et : {perimetre:.2f}" ) 
