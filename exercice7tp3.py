d7678df2f2e94787c5acce28919095ae85f72c18
Rami Ferjani <ramiferjani.20@gmail.com>
2025-02-05 20:16:43 +0000

    Delete exercice7tp3.py

--- exercice7tp3.py
+++
@@ -1,7 +0,0 @@
-a = int  ( input (" tapez le valeur de n :  " ) )
-print (f" la table de multiplication de {a} : " ) 
-for i in  range ( 1 , 11) :
-       print (f" {i} * {a} = { a * i } " )
-       for b in range ( 1 , 10 ) :
-           print (f" {i} * {a} = { a * i } " )
-       
